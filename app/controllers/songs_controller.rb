c530438086acf683fcab692104a0fb9e184be08f
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-10 01:53:37 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,14 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    # If the Artist params are valid && Artist doesn't exist...
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      # Redirect to Artist index, alert user
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      # Song instance becomes a new song with an assigned artist_id
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,7 +46,23 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    # If the artist_id params are valid...
+    if params[:artist_id]
+      # Find the artist_id, assign the instance to the artist_id
+      @artist = Artist.find_by(id: params[:artist_id])
+      # If the artist doesn't exist...
+      if @artist.nil?
+        # Redirect to the artists index, alert the user
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        # Make sure the song belongs to the author, then assign it to the instance
+        @song = @artist.songs.find_by(id: params[:id])
+        # If the song isn't found, alert the user. Redirect to the artist songs index
+        redirect_to artist_songs_path(@artist), alert: "Song not found" if @song.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +87,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,16 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+  
+  def artist_select(artist, song)
+    # If the artist doesn't exist...
+    if artist.nil?
+      # Create a select_tag, with options for a checkbox selection of Artists.all, by :id, list :name
+      select_tag "song[artist_id]", 
+      options_from_collection_for_select(Artist.all, :id, :name) 
+    else
+      # Assign the current song to the new artist being entered by artist_id with a hidden_field_tag
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
